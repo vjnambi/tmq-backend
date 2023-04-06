ba08ea712d208e1fb17bf69dcd860499009561be
vjnambi <vjnambi@gmail.com>
2023-04-05 19:18:58 -0500

    Revert "removed bcrypt related files"
    This reverts commit b19424d75c1a894d14714a95cd1168d5372ed7e8.

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const db = require("./models");
 const cors = require("cors");
 
 const indexRouter = require("./routes/index")
-
+const registerRouter = require("./routes/register")
+const loginRouter = require("./routes/login")
 
 var app = express();
 
@@ -24,7 +25,8 @@ app.use(cors());
 
 
 app.use("/", indexRouter)
-
+app.use("/register", registerRouter)
+app.use("/login", loginRouter)
 
 
 // catch 404 and forward to error handler
