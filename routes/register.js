9d0222e9630d1c0ae8867fe581dd2e55d0c4355e
vjnambi <vjnambi@gmail.com>
2023-04-24 18:18:52 -0500

    check for already existing user in register

--- routes/register.js
+++ routes/register.js
@@ -5,21 +5,28 @@ const crypto = require("crypto")
 
 router.post("/", async (req, res) => {
     const {username, password} = req.body
-    salt = crypto.randomBytes(16).toString('hex'); 
-  
-    // Hashing user's salt and password with 1000 iterations, 
-     
-    hash = crypto.pbkdf2Sync(password, salt,  
-    1000, 64, `sha512`).toString(`hex`); 
-    
 
-    users.create({
-        username: username,
-        password: hash,
-        salt: salt
-    })
-   
-    res.json(`Successfully created`)
+    var present = await users.findOne({where: {username: username}})
+
+    if(!present){
+        salt = crypto.randomBytes(16).toString('hex'); 
+    
+        // Hashing user's salt and password with 1000 iterations, 
+        
+        hash = crypto.pbkdf2Sync(password, salt,  
+        1000, 64, `sha512`).toString(`hex`); 
+        
+
+        users.create({
+            username: username,
+            password: hash,
+            salt: salt
+        })
+    
+        res.json(`Successfully created`)
+    } else {
+        res.json(`Username already taken`)
+    }
 });
 
 
